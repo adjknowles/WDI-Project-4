e0fcb4a9db4fda943579ab5bb886a2d98c870c28
adjknowles <alastair_knowles@yahoo.co.uk>
2015-12-16 13:40:59 +0000

    Made changes to the infowindow on the map

--- api/db/seeds.js
+++ api/db/seeds.js
@@ -102,12 +102,12 @@ recommendation7.save(function(err, recommendation) {
 //Bob
 
 var recommendation8 = new Recommendation({
-  category: "Parks",
-  description: "Museum Gardens",
-  url: "http://www.yorkshiremuseum.org.uk/york-museum-gardens/",
-  latitude: 53.960972,
-  longitude: -1.086655
-})
+  category: "Museum",
+  description: "York Castle Museum",
+  url: "http://www.yorkcastlemuseum.org.uk/",
+  latitude: 53.955366,
+  longitude: -1.078255
+}) 
 
 recommendation8.save(function(err, recommendation) {
  if (err) return console.log(err);
@@ -115,14 +115,66 @@ recommendation8.save(function(err, recommendation) {
 })
 
 var recommendation9 = new Recommendation({
-  category: "Parks",
-  description: "Museum Gardens",
-  url: "http://www.yorkshiremuseum.org.uk/york-museum-gardens/",
-  latitude: 53.960972,
-  longitude: -1.086655
+  category: "Museum",
+  description: "National Railway Museum",
+  url: "http://www.nrm.org.uk/",
+  latitude: 53.960723,
+  longitude: -1.096366
 })
 
 recommendation9.save(function(err, recommendation) {
+ if (err) return console.log(err);
+ console.log("Recommendation saved! ", recommendation);
+})
+
+var recommendation10 = new Recommendation({
+  category: "Attractions",
+  description: "Clifford's Tower",
+  url: "http://www.english-heritage.org.uk/visit/places/cliffords-tower-york/?gclid=CKmdoIGp4MkCFUT3wgodtP4PPw&gclsrc=aw.ds",
+  latitude: 53.955971,
+  longitude: -1.079919
+})
+
+recommendation10.save(function(err, recommendation) {
+ if (err) return console.log(err);
+ console.log("Recommendation saved! ", recommendation);
+})
+
+var recommendation11 = new Recommendation({
+  category: "Cafes",
+  description: "Spring Espresso",
+  url: "http://www.springespresso.co.uk/",
+  latitude: 53.958512,
+  longitude: -1.078745
+})
+
+recommendation11.save(function(err, recommendation) {
+ if (err) return console.log(err);
+ console.log("Recommendation saved! ", recommendation);
+})
+
+var recommendation12 = new Recommendation({
+  category: "Restaurants",
+  description: "The Blue Bicycle",
+  url: "http://www.thebluebicycle.com/",
+  latitude: 53.958103,
+  longitude: -1.077959
+})
+
+recommendation12.save(function(err, recommendation) {
+ if (err) return console.log(err);
+ console.log("Recommendation saved! ", recommendation);
+})
+
+var recommendation13 = new Recommendation({
+  category: "Parks",
+  description: "Rowntree Park",
+  url: "https://www.york.gov.uk/directory_record/376/rowntree_park",
+  latitude: 53.948550,
+  longitude: -1.081871
+})
+
+recommendation13.save(function(err, recommendation) {
  if (err) return console.log(err);
  console.log("Recommendation saved! ", recommendation);
 })
@@ -152,6 +204,13 @@ user1.recommendations.push(recommendation4);
 user1.recommendations.push(recommendation5);
 user1.recommendations.push(recommendation6);
 user1.recommendations.push(recommendation7);
+
+user2.recommendations.push(recommendation8);
+user2.recommendations.push(recommendation9);
+user2.recommendations.push(recommendation10);
+user2.recommendations.push(recommendation11);
+user2.recommendations.push(recommendation12);
+user2.recommendations.push(recommendation13);
 
 user1.save(function(err, user) {
  if (err) return console.log(err);
